8c99eba826c2b954355ecc178ac9b1c92bc4b95f
greydoubt <seanalexander@uchicago.edu>
2021-02-17 22:35:21 +0000

    fixed lat/long

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ from ast import literal_eval #allows loading a list from a file with brackets in
 # File Format Assumptions
 file_name = "input.txt" 
 
-#1  # Feature 0:
-#2  # [[x_0_0, y_0_0], [x_0_1, y_0_1], [x_0_2, y_0_2], ... [x_0_n0, y_0_n0]]
-#  .
-#  .
-#  .
+### start of file
+# 1 # Feature 0:
+# 2 # [[x_0_0, y_0_0], [x_0_1, y_0_1], [x_0_2, y_0_2], ... [x_0_n0, y_0_n0]]
+
+# . # 
+# . # 
+# . # 
+
 #L-1# Feature M:
-#L  # [[x_m_0, y_m_0], [x_m_1, y_m_1], [x_m_2, y_m_2], ... [x_m_nm, y_m_nm]]
+# L # [[x_m_0, y_m_0], [x_m_1, y_m_1], [x_m_2, y_m_2], ... [x_m_nm, y_m_nm]]
+### end of file
 
 # It is further assumed that the data is in correct list-like syntax and contains no missing values.
 
@@ -35,16 +39,21 @@ with open(file_name) as f:
         if count % 2 == 0: 
           mainlist.append([list(literal_eval(line))])
 
-mainlist.pop(0) # using append creates an  empty list, this gets rid of it
+mainlist.pop(0) # this gets rid of the empty sub-list that we appended to
 
 # test statements:
 print("Examples:")
 print((mainlist[0][0][1])) # outputs a pair
-print((mainlist[0][0][1][0])) # outputs a lat
-print((mainlist[0][0][1][1])) # outputs a lon
+print((mainlist[0][0][1][0])) # outputs a longitude (x value)
+print((mainlist[0][0][1][1])) # outputs a latitude (y value)
+
+# use this if you need a count of how many points are in a feature:
+print(len(mainlist[0][0])) #Outputs the number of points that comprise a feature
+
 
 # more test statements:
 #print(mainlist[0]) #outputs all of Feature 0
+
 #print(mainlist[1]) #outputs all of Feature 1
 #print(mainlist[2]) #outputs all of Feature 2
 #print(mainlist[3]) #outputs all of Feature 3
@@ -55,4 +64,4 @@ print((mainlist[0][0][1][1])) # outputs a lon
 # a : Feature ("Feature 0")
 # b : Variables (just has 0, 0 is the coordinates as a "collection", no other Feature-level variables are in this ontology)
 # c : Cartesian Coordinate Pairs (this is many coordinates, can be 0 to the max # points)
-# d : lat/x & lon/y  (0 is x, 1 is y)
+# d : long/x & lat/y  (0 is x, 1 is y)
